3c07a453a83456ec94f64482c137f1b91ab0fbb4
Mohnish <mohnish.a007@gmail.com>
2024-12-24 19:39:40 +0000

    Variables bool type

--- 02variables/main.go
+++ 02variables/main.go
@@ -8,4 +8,7 @@ func main() {
 	var username string = "ABC"
 	fmt.Println(username)
 	fmt.Printf(" Variable type is %T\n", username)
+	var isLog bool = true
+	fmt.Println(isLog)
+	fmt.Printf(" Variable type is %T\n", isLog)
 }
