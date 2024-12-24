f7839fc434ca8b01c705d0dd518cf317fffc90e4
Mohnish <mohnish.a007@gmail.com>
2024-12-24 19:37:56 +0000

    Variables string type

--- 02variables/main.go
+++ 02variables/main.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("Variables here")
+	// String variables
+	var username string = "ABC"
+	fmt.Println(username)
+	fmt.Printf(" Variable type is %T\n", username)
 }
