265cb9af1876b693a40dff81126b96f64bcab645
Mani007 <mohnish.a007@gmail.com>
2026-01-13 20:59:13 +0100

    some variable code added

--- 02variables/main.go
+++ 02variables/main.go
@@ -2,34 +2,20 @@ package main
 
 import "fmt"
 
-// token1 := "asdasd" // you cannot do this outside the function scope
-
-// var token1 = "asdasd" // you can do this outside the function scope
-
-const Logintoken string = "dfadfa" // Contant with capital letters are public and small letter are private by scope in go
 func main() {
-	fmt.Println("Variables here")
-	// String variables
-	var username string = "ABC"
-	fmt.Println(username)
-	fmt.Printf(" Variable type is %T\n", username)
-	var isLog bool = true
-	fmt.Println(isLog)
-	fmt.Printf(" Variable type is %T\n", isLog)
-	var someint int32 = 25
-	fmt.Println(someint)
-	fmt.Printf(" Variable type is %T\n", someint)
-	// Default value for un initialized int
-	var defaultval int32
-	fmt.Println(defaultval) // it will be zero
-	fmt.Printf(" Variable type is %T\n", defaultval)
-	// inplicit type
-	var webadd = "abc" // you cannot use '' insted use ""
-	println(webadd)    // its string
-	webadd = "new"     // you can chage the value but not the type
-	//webadd = 25 // error type cannot be chaged
-	// No var style
-	token := "asasda" // declared without the var keyword
-	println(token)
-	println(Logintoken)
+
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple" // This looks like walrus operator in python
+	fmt.Println(f)
 }
